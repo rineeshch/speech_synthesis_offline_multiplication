793a8dc278e11f43955a03e84ad5cdfaf58c08fd
Rineesh CH <42416458+rineeshch@users.noreply.github.com>
2022-02-02 03:16:24 +0530

    initial commit

---
+++ speech_synthesis_offline_multiplication.py
@@ -0,0 +1,23 @@
+import pyttsx3
+
+n = input("Enter a number")
+dic = {
+    "1" : "ones",
+    "2" : "twos",
+    "3" : "threes",
+    "4" : "fours",
+    "5" : "fives",
+    "6" : "sixes",
+    "7" : "sevens",
+    "8" : "eights",
+    "9" : "nines",
+    "10" : "tens"
+}
+txt = ""
+for i in range (1,11):
+    mul = i*int(n)
+    txt+= str(i) + " " + dic[n] + " are " + str(mul) + "\n"
+
+engine=pyttsx3.init()
+engine.say(txt)
+engine.runAndWait()
